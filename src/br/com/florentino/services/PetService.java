22316e25d1c283dcc14621b93203528bc086ae67
Florentino Cazé <florentinocasn@gmail.com>
2026-07-08 16:43:06 -0300

    fix: always validate city and street even when address number is blank

--- src/br/com/florentino/services/PetService.java
+++ src/br/com/florentino/services/PetService.java
@@ -134,8 +134,6 @@ public class PetService {
 
             if (number.isBlank()) {
                 number = Constants.NOT_INFORMED;
-
-                return new Address(city, street, number);
             }
 
             Address address = new Address(city, street, number);
