6fa238e5ae562a703b67207ca278eadbce63f87b
Florentino Cazé <florentinocasn@gmail.com>
2026-07-07 22:16:11 -0300

    feat: add format number method to string formatter util

--- src/br/com/florentino/utils/StringFormatter.java
+++ src/br/com/florentino/utils/StringFormatter.java
@@ -1,9 +1,16 @@
 package br.com.florentino.utils;
 
+import java.text.DecimalFormat;
 import java.text.Normalizer;
 import java.util.regex.Pattern;
 
 public class StringFormatter {
+    public static String formatNumber(double value) {
+        DecimalFormat formatter = new DecimalFormat("0.##");
+
+        return formatter.format(value);
+    }
+
     public static String removeAccents(String text) {
         text = Normalizer.normalize(text, Normalizer.Form.NFD);
         Pattern pattern = Pattern.compile("\\p{InCombiningDiacriticalMarks}+");
@@ -11,4 +18,5 @@ public class StringFormatter {
 
         return text;
     }
+
 }
