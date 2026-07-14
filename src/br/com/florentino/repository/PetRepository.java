cc84269e429014cb3e1137e43187c952faecbe27
Florentino Cazé <florentinocasn@gmail.com>
2026-07-14 16:09:46 -0300

    feat: add update and editLine to PetRepository

--- src/br/com/florentino/repository/PetRepository.java
+++ src/br/com/florentino/repository/PetRepository.java
@@ -7,11 +7,13 @@ import br.com.florentino.enums.Type;
 import br.com.florentino.utils.Constants;
 import br.com.florentino.utils.NumberParser;
 import br.com.florentino.utils.StringFormatter;
+import br.com.florentino.validator.PetValidator;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -172,5 +174,99 @@ public class PetRepository {
         }
 
         return new ArrayList<>();
+    }
+
+    public void update(Pet pet, String option, String replacement) {
+        Path petFilePath = Paths.get(pet.getFilePath());
+
+        String newLine;
+
+        switch (option) {
+            case "1" -> {
+                pet.setName(replacement);
+
+                newLine = "1 – " + pet.getName();
+
+                editLine(petFilePath, 0, newLine);
+            }
+            case "2" -> {
+                pet.getAddress().setCity(replacement);
+
+                newLine = "4 – " + pet.getAddress().getStreet() + ", " + pet.getAddress().getNumber() + ", " + pet.getAddress().getCity();
+
+                editLine(petFilePath, 3, newLine);
+            }
+            case "3" -> {
+                if (replacement.isBlank()) {
+                    pet.getAddress().setNumber(Constants.NOT_INFORMED);
+                } else {
+                    pet.getAddress().setNumber(replacement);
+                }
+
+                newLine = "4 – " + pet.getAddress().getStreet() + ", " + pet.getAddress().getNumber() + ", " + pet.getAddress().getCity();
+
+                editLine(petFilePath, 3, newLine);
+            }
+            case "4" -> {
+                pet.getAddress().setStreet(replacement);
+
+                newLine = "4 – " + pet.getAddress().getStreet() + ", " + pet.getAddress().getNumber() + ", " + pet.getAddress().getCity();
+
+                editLine(petFilePath, 3, newLine);
+            }
+            case "5" -> {
+                if (replacement == null) {
+                    pet.setAge(null);
+                } else {
+                    pet.setAge(NumberParser.parseDouble(replacement));
+                }
+
+                if (pet.getAge() == null) {
+                    newLine = "5 – " + Constants.NOT_INFORMED;
+                } else {
+                    newLine = "5 – " + StringFormatter.formatNumber(pet.getAge()) + " anos";
+                }
+
+                editLine(petFilePath, 4, newLine);
+            }
+            case "6" -> {
+                if (replacement == null) {
+                    pet.setWeight(null);
+                } else {
+                    pet.setWeight(NumberParser.parseDouble(replacement));
+                }
+
+                if (pet.getWeight() == null) {
+                    newLine = "6 – " + Constants.NOT_INFORMED;
+                } else {
+                    newLine = "6 – " + StringFormatter.formatNumber(pet.getWeight()) + " kg";
+                }
+
+                editLine(petFilePath, 5, newLine);
+            }
+            case "7" -> {
+                pet.setRace(replacement);
+
+                newLine = "7 – " + pet.getRace();
+
+                editLine(petFilePath, 6, newLine);
+            }
+        }
+    }
+
+    private void editLine(Path petFilePath, int lineToEdit, String lineToReplace) {
+        try {
+            List<String> lines = Files.readAllLines(petFilePath, StandardCharsets.UTF_8);
+
+            if (lineToEdit < lines.size()) {
+                lines.set(lineToEdit, lineToReplace);
+            }
+
+            Files.write(petFilePath, lines, StandardCharsets.UTF_8);
+
+            System.out.println("Dado alterado com sucesso!");
+        } catch (IOException e) {
+            System.err.println("Erro ao alterar dado.");
+        }
     }
 }
