6997c1a85da3f3568d4e63af66ba1b921376471b
Florentino Cazé <florentinocasn@gmail.com>
2026-07-15 16:57:14 -0300

    feat: add appendToForm to FormRepository and addQuestionToForm() to FormService

--- src/br/com/florentino/repository/FormRepository.java
+++ src/br/com/florentino/repository/FormRepository.java
@@ -2,13 +2,39 @@ package br.com.florentino.repository;
 
 import br.com.florentino.utils.Constants;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class FormRepository {
     public List<String> readForm() throws IOException {
         return Files.readAllLines(Path.of(Constants.FORM_PATH));
+    }
+
+    public void appendToForm(String question) {
+        Path formFilePath = Paths.get(Constants.FORM_PATH);
+
+        long fileLinesAmount;
+
+        try (Stream<String> lines = Files.lines(formFilePath)) {
+            fileLinesAmount = lines.count();
+        } catch (IOException e) {
+            System.out.println("Erro ao ler arquivo.");
+            return;
+        }
+
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(formFilePath.toFile(), StandardCharsets.UTF_8, true))) {
+            bufferedWriter.newLine();
+            bufferedWriter.write((fileLinesAmount + 1) + " – " + question);
+        } catch (IOException e) {
+            System.out.println("Erro ao adicionar pergunta ao formulário.");
+        }
     }
 }
--- src/br/com/florentino/services/FormService.java
+++ src/br/com/florentino/services/FormService.java
@@ -4,15 +4,42 @@ import br.com.florentino.repository.FormRepository;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Scanner;
 
 public class FormService {
     private final FormRepository formRepository;
+    private final Scanner scanner;
 
-    public FormService(FormRepository formRepository) {
+    public FormService(FormRepository formRepository, Scanner scanner) {
         this.formRepository = formRepository;
+        this.scanner = scanner;
     }
 
     public List<String> getQuestions() throws IOException {
         return formRepository.readForm();
+    }
+
+    public void addQuestionToForm() {
+        while (true) {
+            System.out.print("Insira a pergunta que você deseja adicionar ao formulário: ");
+            String questionToAdd = scanner.nextLine();
+
+            formRepository.appendToForm(questionToAdd);
+
+            System.out.println("Você deseja fazer mais alguma mudança?");
+            System.out.println("[1] Sim;");
+            System.out.println("[2] Não.");
+            System.out.print("Escolha uma opção: ");
+
+            String option = scanner.nextLine();
+
+            if (option.equals("2")) {
+                break;
+            }
+
+            if (!option.equals("1")) {
+                System.out.println("Opção inválida.");
+            }
+        }
     }
 }
